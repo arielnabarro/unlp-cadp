721ead8254bf89eb92de8a591d8fc63bb968b10e
arielnabarro <arielnabarro95@gmail.com>
2023-05-11 23:56:48 -0300

    Ejercicio 6 resuelto

--- practica1_parte2.md
+++ practica1_parte2.md
@@ -265,7 +265,52 @@
     * La cantidad de productos de más de 16 pesos con código par.
 
     ```pascal
+    program seis;
 
+    var
+        codigo, codigo_a, codigo_b, par, i: integer;
+        precio, precio_a, precio_b: real;
+    begin
+            repeat
+                write('1.Ingrese código de producto:');
+                read(codigo_a);
+                codigo_b:= codigo_a;
+            until (codigo_a<=200);
+            write('1.Ingrese precio de producto:');
+            read(precio_a);
+            precio_b:= precio_a;   
+        for i := 2 to 5 do
+        begin
+            repeat
+                write(i,'.Ingrese código de producto:');
+                read(codigo);
+            until (codigo<=200);
+            write(i,'.Ingrese precio de producto:');
+            read(precio);
+            if precio<precio_a then
+                begin
+                precio_b:= precio_a;
+                precio_a:= precio;
+                codigo_b:=codigo_a;
+                codigo_a:=codigo;
+                end
+            else
+                begin
+                    if precio < precio_b then
+                        begin
+                        precio_b:= precio;
+                        codigo_b:= codigo;
+                        end;
+                end;
+            if precio>16 then  {ver si es par}
+            begin
+            if codigo mod 2 = 0 then
+                par:= par+1            
+            end;
+        end;
+        writeln('El producto mas barato es: ',codigo_a,'seguido del: ',codigo_b);
+        writeln('La cantidad de productos con codigo par mayores de $16 es: ',par);
+    end.
     ```
 
 7. Realizar un programa que lea desde teclado información de autos de carrera. Para cada uno de los autos se lee el nombre del piloto y el tiempo total que le tomó finalizar la carrera. En la carrera participaron 100 autos. Informar en pantalla:
--- programa.pas
+++ programa.pas
@@ -5,45 +5,51 @@
     * Los códigos de los dos productos más baratos.
     * La cantidad de productos de más de 16 pesos con código par.
 }
-
 program seis;
 
 var
-    codigo, codigo_a, codigo_b, par: integer;
+    codigo, codigo_a, codigo_b, par, i: integer;
     precio, precio_a, precio_b: real;
 begin
         // es posible que tenga que usar tipos de datos con rangos.
         // tambien que realice algun tipo de validación con logica booleana en un if.
         repeat
-            write( 'Ingrese código de producto:');
-            read(codigo);
-        until (codigo<=200);
-        write( 'Ingrese precio de producto:');
-        read(precio);     
-    for i := 2 to 200 do
+            write('1.Ingrese código de producto:');
+            read(codigo_a);
+            codigo_b:= codigo_a;
+        until (codigo_a<=200);
+        write('1.Ingrese precio de producto:');
+        read(precio_a);
+        precio_b:= precio_a;   
+    for i := 2 to 5 do
     begin
         repeat
-            write( 'Ingrese código de producto:');
+            write(i,'.Ingrese código de producto:');
             read(codigo);
         until (codigo<=200);
-        write( 'Ingrese precio de producto:');
+        write(i,'.Ingrese precio de producto:');
         read(precio);
         if precio<precio_a then
             begin
-                precio_b:= precio_a;
-                precio_a= precio;
-            end;
+            precio_b:= precio_a;
+            precio_a:= precio;
+            codigo_b:=codigo_a;
+            codigo_a:=codigo;
+            end
         else
             begin
                 if precio < precio_b then
-                    precio_b:= precio;    
+                    begin
+                    precio_b:= precio;
+                    codigo_b:= codigo;
+                    end;
             end;
-        if precio>16 then
+        if precio>16 then  {ver si es par}
         begin
-        if precio mod 2 = 0 then {ver si es par}
-            par:= par+1;            
+        if codigo mod 2 = 0 then
+            par:= par+1            
         end;
     end;
-    writeln('El producto mas barato es: ',)
+    writeln('El producto mas barato es: ',codigo_a,'seguido del: ',codigo_b);
+    writeln('La cantidad de productos con codigo par mayores de $16 es: ',par);
 end.
-    
